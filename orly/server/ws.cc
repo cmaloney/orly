b3b277e44cfb43d317c51aeeacae177f771f7684
Jason Lucas <jlucas@orlyatomcs.com>
2014-05-08 13:34:03 -0700

    Websockets interface through to the point where we can make a call to the server and get a response.  This is still isolated from orlyi at the moment, running as a new object with injected dependencies representing the rest of the server.

---
+++ orly/server/ws.cc
@@ -0,0 +1,333 @@
+/* <orly/server/ws.cc>
+
+   Implements <orly/server/ws.h>.
+
+   Copyright 2010-2014 OrlyAtomics, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+#include <orly/server/ws.h>
+
+#include <cassert>
+#include <map>
+#include <memory>
+#include <thread>
+#include <sstream>
+#include <string>
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
+#include <orly/client/program/parse_stmt.h>
+#include <orly/client/program/translate_expr.h>
+#include <orly/indy/key.h>
+#include <orly/sabot/state_dumper.h>
+#include <orly/sabot/type_dumper.h>
+
+using namespace std;
+using namespace std::placeholders;
+
+using namespace Base;
+
+using namespace Orly;
+using namespace Orly::Client::Program;
+using namespace Orly::Sabot;
+using namespace Orly::Server;
+
+/* The implementation of the TWs interface declared in the header. */
+class TWsImpl final
+    : public TWs {
+  public:
+
+  /* Starts up the server. */
+  TWsImpl(TSessionManager *session_mngr, in_port_t port_number)
+      : SessionManager(session_mngr) {
+    assert(session_mngr);
+    AsioServer.clear_access_channels(websocketpp::log::alevel::all);
+    AsioServer.init_asio();
+    AsioServer.set_open_handler(bind(&TWsImpl::OnOpen, this, _1));
+    AsioServer.set_close_handler(bind(&TWsImpl::OnClose, this, _1));
+    AsioServer.set_message_handler(bind(&TWsImpl::OnMsg, this, _1, _2));
+    AsioServer.listen(port_number);
+    AsioServer.start_accept();
+    BgThread = thread(&TWsImpl::BgMain, this);      
+  }
+
+  /* Shuts down the server. */
+  virtual ~TWsImpl() {
+    assert(this);
+    AsioServer.stop();
+    BgThread.join();
+  }
+
+  private:
+
+  /* Pull in some types from the websocket ASIO implementation. */
+  using TAsioServer = websocketpp::server<websocketpp::config::asio>;
+  using TMsgPtr = TAsioServer::message_ptr;
+  using TConnHndl = websocketpp::connection_hdl;
+
+  /* TODO */
+  class TConn final {
+    NO_COPY(TConn);
+    public:
+
+    /* TODO */
+    TConn(TWsImpl *ws, TConnHndl hndl)
+        : Ws(ws), Hndl(hndl), Exiting(false) {}
+
+    /* TODO */
+    ~TConn() {}
+
+    /* TODO */
+    bool IsExiting() const noexcept {
+      assert(this);
+      return Exiting;
+    }
+
+    /* TODO */
+    string OnMsg(TMsgPtr msg) {
+      assert(this);
+      assert(msg);
+      ostringstream strm;
+      ParseStmtStr(
+          msg->get_payload().c_str(),
+          [this, &strm](const TStmt *stmt) {
+             stmt->Accept(TStmtVisitor(this, strm));
+          }
+      );
+      return strm.str();
+    }
+
+    private:
+
+    /* TODO */
+    class TStmtVisitor final
+        : public TStmt::TVisitor {
+      public:
+
+      /* TODO */
+      TStmtVisitor(TConn *conn, ostream &strm)
+          : Conn(conn), Strm(strm) {}
+
+      /* Not supported. */
+      virtual void operator()(const TBeginImportStmt *) const override { NotSupported("begin import"); }
+      virtual void operator()(const TEndImportStmt   *) const override { NotSupported("end import"); }
+      virtual void operator()(const TImportStmt      *) const override { NotSupported("import"); }
+      virtual void operator()(const TInstallStmt     *) const override { NotSupported("install"); }
+      virtual void operator()(const TPovStatusStmt   *) const override { NotSupported("pause/unpause pov"); }
+      virtual void operator()(const TSetTtlStmt      *) const override { NotSupported("set ttl"); }
+      virtual void operator()(const TSetUserIdStmt   *) const override { NotSupported("set user_id"); }
+      virtual void operator()(const TTailStmt        *) const override { NotSupported("tail"); }
+      virtual void operator()(const TUninstallStmt   *) const override { NotSupported("uninstall"); }
+
+      /* Echo. */
+      virtual void operator()(const TEchoStmt *stmt) const override {
+        assert(this);
+        assert(stmt);
+        void *alloc = alloca(SabotStateSize);
+        TWrapper(NewStateSabot(stmt->GetExpr(), alloc))->Accept(TStateDumper(Strm));
+      }
+
+      /* Exit. */
+      virtual void operator()(const TExitStmt *) const override {
+        assert(this);
+        Conn->Exiting = true;
+      }
+
+      /* New session. */
+      virtual void operator()(const TNewSessionStmt *) const override {
+        assert(this);
+        if (Conn->Session) {
+          throw invalid_argument("session already established");
+        }
+        Conn->Session.reset(Conn->Ws->SessionManager->NewSession());
+        Strm << Conn->Session->GetId();
+      }
+
+      /* Resume session. */
+      virtual void operator()(const TResumeSessionStmt *stmt) const override {
+        assert(this);
+        assert(stmt);
+        if (Conn->Session) {
+          throw invalid_argument("session already established");
+        }
+        Conn->Session.reset(Conn->Ws->SessionManager->ResumeSession(Translate(stmt->GetIdExpr())));
+        Strm << Conn->Session->GetId();
+      }
+
+      /* New pov. */
+      virtual void operator()(const TPovConsStmt *stmt) const override {
+        assert(this);
+        assert(stmt);
+        if (!Conn->Session) {
+          throw invalid_argument("session not yet established");
+        }
+        bool is_safe = dynamic_cast<const TSafeGuarantee *>(stmt->GetPovGuarantee()) != nullptr;
+        bool is_shared = dynamic_cast<const TSharedKind *>(stmt->GetPovKind()) != nullptr;
+        TOpt<TUuid> parent_id;
+        auto parent = dynamic_cast<const TParent *>(stmt->GetOptParent());
+        if (parent) {
+          parent_id = Translate(parent->GetIdExpr());
+        }
+        Strm << Conn->Session->NewPov(is_safe, is_shared, parent_id);
+      }
+
+      /* Try call. */
+      virtual void operator()(const TTryStmt *stmt) const override {
+        assert(this);
+        assert(stmt);
+        if (!Conn->Session) {
+          throw invalid_argument("session not yet established");
+        }
+        TUuid pov_id = Translate(stmt->GetPovId());
+        vector<string> fq_name;
+        TranslatePathName(fq_name, stmt->GetPackage());
+        TClosure closure(stmt->GetMethodName()->GetLexeme().GetText());
+        auto list = dynamic_cast<const TObjMemberList *>(stmt->GetArgs()->GetOptObjMemberList());
+        void *alloc = alloca(SabotStateSize);
+        while (list) {
+          auto member = list->GetObjMember();
+          TWrapper state(NewStateSabot(member->GetExpr(), alloc));
+          closure.AddArgBySabot(member->GetName()->GetLexeme().GetText(), state);
+          auto tail = dynamic_cast<const TObjMemberListTail *>(list->GetOptObjMemberListTail());
+          list = tail ? tail->GetObjMemberList() : nullptr;
+        }
+        TMethodResult result = Conn->Session->Try(TMethodRequest(pov_id, fq_name, closure));
+        Indy::TKey(result.GetValue(), result.GetArena().get()).Dump(Strm);
+      }
+
+      private:
+
+      /* The size used to alloca() space for a sabot of state. */
+      static constexpr auto SabotStateSize = Orly::Sabot::State::GetMaxStateSize();
+
+      /* Pull in some stuff from sabot. */
+      using TStateDumper = Orly::Sabot::TStateDumper;
+      using TWrapper = Orly::Sabot::State::TAny::TWrapper;
+
+      /* TODO */
+      static void NotSupported(const char *name) {
+        assert(name);
+        ostringstream strm;
+        strm << '"' << name << "\" statement not supported via websocket";
+        throw invalid_argument(strm.str());
+      }
+
+      /* Translate an Orlyscript id into a TUuid. */
+      static TUuid Translate(const TIdExpr *id_expr) {
+        assert(id_expr);
+        return TUuid(id_expr->GetLexeme().GetText().substr(1, id_expr->GetLexeme().GetText().size() - 2).c_str());
+      }
+
+      /* TODO */
+      TConn *Conn;
+
+      /* TODO */
+      ostream &Strm;
+
+    };  // TWsImpl::TStmtVisitor
+
+    /* TODO */
+    TWsImpl *Ws;
+
+    /* TODO */
+    TConnHndl Hndl;
+
+    /* TODO */
+    bool Exiting;
+
+    /* TODO */
+    unique_ptr<TSessionPin> Session;
+
+  };  // TWsImpl::TConn
+
+  /* The entry point of the background thread launched from the ctor.
+     This will run until stopped by the dtor. */
+  void BgMain() {
+    assert(this);
+    AsioServer.run();
+    for (const auto &item: Conns) {
+      delete item.second;
+    }
+  }
+
+  /* TODO */
+  void OnOpen(TConnHndl conn_hndl) {
+    assert(this);
+    unique_ptr<TConn> temp(new TConn(this, conn_hndl));
+    if (!Conns.insert(make_pair(conn_hndl, temp.get())).second) {
+      throw invalid_argument("cannot reopen open connection");
+    }
+    temp.release();
+  }
+   
+  /* TODO */
+  void OnClose(TConnHndl conn_hndl) {
+    assert(this);
+    auto iter = Conns.find(conn_hndl);
+    if (iter == Conns.end()) {
+      throw invalid_argument("cannot close unopen connection");
+    }
+    delete iter->second;
+    Conns.erase(iter);
+  }
+
+  /* Invoked from a background thread each time a websocket message arrives. */
+  void OnMsg(TConnHndl conn_hndl, TMsgPtr msg) {
+    assert(this);
+    /* Find the connection object established by the OnOpen() call. */
+    auto iter = Conns.find(conn_hndl);
+    if (iter == Conns.end()) {
+      throw invalid_argument("cannot use unopen connection");
+    }
+    /* Pass the message to the connection object for processing. */
+    TConn *conn = iter->second;
+    const char *prefix;
+    string reply;
+    try {
+      reply = conn->OnMsg(msg);
+      prefix = "ok";
+    } catch (const exception &ex) {
+      reply = ex.what();
+      prefix = "ex";
+    }
+    /* Format the reply and send it back to the client. */
+    ostringstream strm;
+    strm << prefix;
+    if (!reply.empty()) {
+      strm << ':' << reply;
+    }
+    AsioServer.send(conn_hndl, strm.str(), websocketpp::frame::opcode::text);    
+    if (conn->IsExiting()) {
+      AsioServer.close(conn_hndl, websocketpp::close::status::normal, "");
+    }
+  }
+
+  /* The session manager interface passed to us at construction time. */
+  TSessionManager *SessionManager;
+
+  /* This object handles the mechanics of the websocket protocol. */
+  TAsioServer AsioServer;
+
+  /* The background thread launched by the ctor. */
+  thread BgThread;
+
+  /* The sessions currently open. */
+  map<TConnHndl, TConn *, owner_less<TConnHndl>> Conns;
+
+};  // TWsImpl
+
+TWs *TWs::New(TSessionManager *session_mngr, in_port_t port_number) {
+  return new TWsImpl(session_mngr, port_number);
+}
---
+++ orly/server/ws.h
@@ -0,0 +1,105 @@
+/* <orly/server/ws.h>
+
+   The websockets server object used by the main server.
+
+   Copyright 2010-2014 OrlyAtomics, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+#pragma once
+
+#include <netinet/in.h>
+
+#include <base/class_traits.h>
+#include <base/opt.h>
+#include <base/uuid.h>
+#include <orly/method_request.h>
+#include <orly/method_result.h>
+
+namespace Orly {
+
+  namespace Server {
+
+     /* An interface to a websocket server.  We use an interface-only definition
+        here to avoid bloating up the build with a lot of websocket-specific
+        headers. */
+      class TWs {
+        NO_COPY(TWs);
+        public:
+
+        /* Inherit and finalize this class to handle per-session operations. */
+        class TSessionPin {
+          NO_COPY(TSessionPin);
+          public:
+
+          /* Do-little. */
+          virtual ~TSessionPin() {}
+
+          /* The id used to resume the session later. */
+          virtual const Base::TUuid &GetId() const = 0;
+
+          /* TODO */
+          virtual Base::TUuid NewPov(bool is_safe, bool is_shared, const Base::TOpt<Base::TUuid> &parent_id) const = 0;
+
+          /* TODO */
+          virtual TMethodResult Try(const TMethodRequest &method_request) const = 0;
+
+          protected:
+
+          /* Do-little. */
+          TSessionPin() {}
+
+        };  // TWs::TSessionPin
+
+        /* Inherit and finalize this class to handle per-connection. */
+        class TSessionManager {
+          NO_COPY(TSessionManager);
+          public:
+
+          /* For the convenience of those who inherit from us. */
+          using TSessionPin = TWs::TSessionPin;
+
+          /* Called when the connection wishes to create a new session. */
+          virtual TSessionPin *NewSession() = 0;
+
+          /* Called when the connection wishes to resume an old session. */
+          virtual TSessionPin *ResumeSession(const Base::TUuid &id) = 0;
+
+          protected:
+
+          /* Do-little. */
+          TSessionManager() {}
+
+          /* Do-little. */
+          virtual ~TSessionManager() {}
+
+        };  // TWs::TSessionManager
+
+        /* Shut down the server. */
+        virtual ~TWs() {}
+
+        /* Use this factory to construct an instance of this class.
+           The server will be open for business by the time this function
+           returns.  To shut down the server, destroy the object. */
+        static TWs *New(TSessionManager *session_mngr, in_port_t port_number = 8080);
+
+        protected:
+
+        /* Start up the server. */
+        TWs() {}
+
+    };  // TWs
+
+  }  // Server
+
+}  // Orly
