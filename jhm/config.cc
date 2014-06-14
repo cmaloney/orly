8326bd2bfc0b4b24d1c675246a2f2b5a2fa88258
Cody Maloney <cmaloney@theoreticalchaos.com>
2014-06-14 00:50:24 -0700

    jhm: Remove bad / broken delta configration

--- jhm/config.cc
+++ jhm/config.cc
@@ -30,46 +30,6 @@ TConfig::TConfig(TJson base_config) {
   ConfigStack.emplace_front(move(base_config));
 }
 
-// Returns true if it added at least one item to entry.
-// Searches down the config, finding entries that begin with the chunk key ('+'/'-'/'=' prefix just means deltas for the key)
-// entry must be either Null, in which case we can promote it arbitrarily or an object (which means we can add).
-// NOTE: Returns true iff the object couldn't be added to by another delta (We crossed a force-assign key or set it to a hard value).
-//TODO: implement delta-based config sections (Add/remove/etc. items from a section explicitly with '+'/'-'/'=' prefix or postfix)
-bool AddDeltas(const TJson &config, const vector<string> &chunks, TJson &entry) {
-  assert(entry.GetKind() == TJson::Null || entry.GetKind() == TJson::Object);
-
-  // Get to the bit we want;
-  const Base::TJson *cur_elem = &config;
-  for(const auto &chunk : chunks) {
-    if (!cur_elem->Contains(chunk)) {
-      return false; // Not in the given config
-    }
-    cur_elem = &((*cur_elem)[chunk]);
-  }
-
-  if (entry.GetKind() == TJson::Object) {
-    if (cur_elem->GetKind() != TJson::Object) {
-      // Found the end (Config overridden with different type).
-      // TODO: Should this be an error?
-      return true;
-    }
-
-    cur_elem->ForEachElem([&entry] (const std::string &key, const TJson &val) -> bool {
-      // Insert if not exists.
-      if (!entry.Contains(key)) {
-        entry[key] = val;
-      }
-      return true;
-    });
-  } else if (entry.GetKind() == TJson::Array) {
-    NOT_IMPLEMENTED_S("Merge multiple arrays at different configuration levels");
-  } else {
-    entry = *cur_elem;
-  }
-
-  return true;
-}
-
 TJson TConfig::GetEntry(const string &name) const {
   TJson ret;
   if (!TryGetEntry(name, ret)) {
@@ -78,27 +38,36 @@ TJson TConfig::GetEntry(const string &name) const {
   return ret;
 }
 
+/* Resolves a scope / sequence of names to a json blob. */
+static const TJson *ResolveName(const TJson *start, const vector<string> &chunks) {
+  // Walk through
+  for(const auto &chunk : chunks) {
+    if (!start->Contains(chunk)) {
+      return nullptr;
+    }
+    start = &((*start)[chunk]);
+  }
+  return start;
+}
 
+//TODO: Switch to vector<string> for name always, rather than splitting apart?
 bool TConfig::TryGetEntry(const string &name, TJson &out) const {
   vector<string> chunks;
   Base::Split(".", name, chunks);
 
-  bool found_something = false;
-  TJson entry;
-
   // For each config, starting at the top of the stack. If the config contains the entry, use it.
-  // If the config contains a section named the entry, use it as a basis.
-  // If any entries are deltas, apply them
-  // Continue going until we get to a hard assign for the entry or any level above it
+  // TODO: Add delta support
   for(auto &config: ConfigStack) {
-    if(found_something |= AddDeltas(config, chunks, entry)) {
-      break;
+    const TJson *val = ResolveName(&config, chunks);
+    if (val) {
+      // NOTE: We have no choice but to copy
+      // We don't return a const pointer to the object because we're going to merge deltas shortly, which means building
+      // a new temporary.
+      out = *val;
+      return true;
     }
   }
-  if (found_something) {
-    out = entry;
-  }
-  return found_something;
+  return false;
 }
 
 void TConfig::AddComputed(TJson &&config) {
