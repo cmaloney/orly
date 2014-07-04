f2ef38abd14c7611e8d2dac017fe3aee4271cc66
jmlvanre <joris.van.remoortere@gmail.com>
2014-07-03 18:00:51 -0700

    Adding specializations for Rt::TOpt to go from native -> sabot -> native

--- orly/sabot/to_native.h
+++ orly/sabot/to_native.h
@@ -812,6 +812,56 @@ namespace Orly {
     };  // TToNativeVisitor
 
     template <typename TVal>
+    class TToNativeVisitor<Rt::TOpt<TVal>> final
+        : public TStateVisitor {
+      NO_COPY(TToNativeVisitor);
+      public:
+      /* TODO */
+      TToNativeVisitor(Rt::TOpt<TVal> &out) : Out(out) {}
+      /* Overrides. */
+      virtual void operator()(const State::TFree &/*state*/) const override       { throw; }
+      virtual void operator()(const State::TTombstone &/*state*/) const override  { throw; }
+      virtual void operator()(const State::TVoid &/*state*/) const override       { throw; }
+      virtual void operator()(const State::TInt8 &/*state*/) const override       { throw; }
+      virtual void operator()(const State::TInt16 &/*state*/) const override      { throw; }
+      virtual void operator()(const State::TInt32 &/*state*/) const override      { throw; }
+      virtual void operator()(const State::TInt64 &/*state*/) const override      { throw; }
+      virtual void operator()(const State::TUInt8 &/*state*/) const override      { throw; }
+      virtual void operator()(const State::TUInt16 &/*state*/) const override     { throw; }
+      virtual void operator()(const State::TUInt32 &/*state*/) const override     { throw; }
+      virtual void operator()(const State::TUInt64 &/*state*/) const override     { throw; }
+      virtual void operator()(const State::TBool &/*state*/) const override       { throw; }
+      virtual void operator()(const State::TChar &/*state*/) const override       { throw; }
+      virtual void operator()(const State::TFloat &/*state*/) const override      { throw; }
+      virtual void operator()(const State::TDouble &/*state*/) const override     { throw; }
+      virtual void operator()(const State::TDuration &/*state*/) const override   { throw; }
+      virtual void operator()(const State::TTimePoint &/*state*/) const override  { throw; }
+      virtual void operator()(const State::TUuid &/*state*/) const override       { throw; }
+      virtual void operator()(const State::TBlob &/*state*/) const override       { throw; }
+      virtual void operator()(const State::TStr &/*state*/) const override        { throw; }
+      virtual void operator()(const State::TDesc &/*state*/) const override       { throw; }
+      virtual void operator()(const State::TOpt &state) const override            {
+        void *pin_alloc = alloca(State::GetMaxStatePinSize());
+        State::TOpt::TPin::TWrapper pin(state.Pin(pin_alloc));
+        if (pin->GetElemCount()) {
+          void *state_alloc = alloca(Sabot::State::GetMaxStateSize());
+          TVal val;
+          ToNative(*Sabot::State::TAny::TWrapper(pin->NewElem(0, state_alloc)), val);
+          Out = Rt::TOpt<TVal>(val);
+        } else {
+          Out.Reset();
+        }
+      }
+      virtual void operator()(const State::TSet &/*state*/) const override        { throw; }
+      virtual void operator()(const State::TVector &/*state*/) const override     { throw; }
+      virtual void operator()(const State::TMap &/*state*/) const override        { throw; }
+      virtual void operator()(const State::TRecord &/*state*/) const override     { throw; }
+      virtual void operator()(const State::TTuple &/*state*/) const override      { throw; }
+      private:
+      Rt::TOpt<TVal> &Out;
+    };  // TToNativeVisitor
+
+    template <typename TVal>
     class TToNativeVisitor<Orly::TDesc<TVal>> final
         : public TStateVisitor {
       NO_COPY(TToNativeVisitor);
