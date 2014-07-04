ad77eabab1744a4703474ae95e51f6097ce77794
Jason Lucas <jlucas@orlyatomcs.com>
2014-07-04 16:34:21 -0700

    Now parsing time points.

--- orly/csv_to_bin/level3.cc
+++ orly/csv_to_bin/level3.cc
@@ -18,6 +18,9 @@
 
 #include <orly/csv_to_bin/level3.h>
 
+#include <iostream>  // TODO: remove me
+#include <limits>
+
 using namespace std;
 using namespace Orly::CsvToBin;
 
@@ -95,15 +98,30 @@ TLevel3 &TLevel3::operator>>(Base::TUuid &that) {
 TLevel3 &TLevel3::operator>>(int64_t &that) {
   assert(this);
   assert(&that);
-  string temp;
-  *this >> temp;
-  that = stoll(temp);
+  uint8_t c = Peek();
+  int64_t sign;
+  if (c == '+') {
+    sign = 1;
+    Pop();
+  } else if (c == '-') {
+    sign = -1;
+    Pop();
+  } else {
+    sign = 1;
+  }
+  int64_t temp;
+  size_t dummy;
+  ReadDecimalInt(temp, dummy);
+  that = temp * sign;
   return *this;
 }
 
 TLevel3 &TLevel3::operator>>(double &that) {
   assert(this);
   assert(&that);
+  /* TODO: Yeah, this is cheating.  We really shouldn't be consuming all the
+     bytes in the field and then translating just the prefix into a number.
+     We should scan property to conserve the rest of the field. */
   string temp;
   *this >> temp;
   that = stod(temp);
@@ -125,6 +143,100 @@ TLevel3 &TLevel3::operator>>(std::string &that) {
 TLevel3 &TLevel3::operator>>(Base::Chrono::TTimePnt &that) {
   assert(this);
   assert(&that);
+  /* Parse something like 2014-07-04. */
+  int64_t year;
+  size_t size;
+  ReadDecimalInt(year, size);
+  uint8_t match = MatchByte("-/");
+  if (TryMatchByte("Ww")) {
+    THROW_ERROR(TNotSupported) << "week number in time point";
+  }
+  int64_t month;
+  ReadDecimalInt(month, size);
+  if (!CanPeek()) {
+    THROW_ERROR(TNotSupported) << "day-of-year number in time point";
+  }
+  MatchByte(match);
+  int64_t day;
+  ReadDecimalInt(day, size);
+  /* Parse something like T15:38:20.14. */
+  int64_t hour = 0, min = 0, sec = 0, nano = 0;
+  if (CanPeek()) {
+    MatchByte(" Tt/");
+    ReadDecimalInt(hour, size);
+    match = MatchByte(":/");
+    if (CanPeek()) {
+      ReadDecimalInt(min, size);
+      MatchByte(match);
+      if (CanPeek()) {
+        ReadDecimalInt(sec, size);
+        if (CanPeek() && Peek() == '.') {
+          Pop();
+          if (CanPeek() && isdigit(Peek())) {
+            ReadDecimalInt(nano, size);
+            if (size >= 10) {
+              THROW_ERROR(TNumberOutOfRange)
+                  << "too many decimals in time point";
+            }
+            size = 9 - size;
+            for (; size > 0; --size) {
+              nano *= 10;
+            }
+          }
+        }
+      }
+    }
+  }
+  int64_t offset = 0;
+  if (CanPeek() && TryMatchByte("Zz+-", match)) {
+    int64_t sign;
+    switch (match) {
+      case '+': {
+        sign = 1;
+        break;
+      }
+      case '-': {
+        sign = -1;
+        break;
+      }
+      default: {
+        sign = 0;
+      }
+    }
+    if (sign) {
+      int64_t hi = 0, lo = 0, temp;
+      ReadDecimalInt(temp, size);
+      switch (size) {
+        case 2: {
+          hi = temp;
+          MatchByte(':');
+          ReadDecimalInt(lo, size);
+          if (size != 2) {
+          THROW_ERROR(TSyntaxError)
+              << "time zone minutes must be a 2-digit number";
+          }
+          break;
+        }
+        case 4: {
+          hi = temp / 100,
+          lo = temp % 100;
+          break;
+        }
+        default: {
+          THROW_ERROR(TSyntaxError)
+              << "time zone must be 4-digit number or "
+                 "a colon-separated pair of 2-digit numbers";
+        }
+      }
+      offset = sign * (hi * 60 + lo);
+    }
+  }
+  cout
+      << year << "/" << month << "/" << day << " "
+      << hour << ":" << min << ":" << sec << " "
+      << nano << " " << offset;
+  that = Base::Chrono::CreateTimePnt(
+      year, month, day, hour, min, sec, nano, offset);
   return *this;
 }
 
@@ -172,6 +284,26 @@ bool TLevel3::AtRecord() const {
   return result;
 }
 
+void TLevel3::MatchByte(uint8_t expected) {
+  assert(this);
+  if (!TryMatchByte(expected)) {
+    THROW_ERROR(TSyntaxError)
+        << "expected '" << expected
+        << "', found '" << static_cast<char>(Peek()) << '\'';
+  }
+}
+
+uint8_t TLevel3::MatchByte(const char *expected) {
+  assert(this);
+  uint8_t match;
+  if (!TryMatchByte(expected, match)) {
+    THROW_ERROR(TSyntaxError)
+        << "expected any of '" << expected
+        << "', found '" << static_cast<char>(Peek()) << '\'';
+  }
+  return match;
+}
+
 void TLevel3::MatchKeyword(const char *keyword) {
   assert(this);
   for (const char *csr = keyword; *csr; ++csr) {
@@ -190,6 +322,34 @@ void TLevel3::MatchState(TLevel2::TState state) {
         << ", found " << TLevel2::GetName(Level2->State);
   }
   ++Level2;
+}
+
+void TLevel3::ReadDecimalInt(int64_t &value, size_t &size) {
+  assert(this);
+  assert(&value);
+  assert(&size);
+  size = 0;
+  __int128 result = 0;
+  uint8_t c;
+  while (CanPeek() && isdigit(c = Peek())) {
+    result = (result * 10) + c - '0';
+    if (result > numeric_limits<int64_t>::max()) {
+      THROW_ERROR(TNumberOutOfRange) << "base-10 number is too big";
+    }
+    Pop();
+    ++size;
+  }
+  if (!size) {
+    if (CanPeek()) {
+      THROW_ERROR(TSyntaxError)
+          << "expected base-10 number, found '"
+          << static_cast<char>(Peek()) << '\'';
+    } else {
+      THROW_ERROR(TSyntaxError)
+          << "expected base-10 number, found end of field";
+    }
+  }
+  value = result;
 }
 
 bool TLevel3::RefreshBytes(bool required) const {
@@ -225,6 +385,33 @@ bool TLevel3::RefreshBytes(bool required) const {
     if (!success && required) {
       THROW_ERROR(TPastEnd);
     }
+  }
+  return success;
+}
+
+bool TLevel3::TryMatchByte(uint8_t expected) {
+  assert(this);
+  bool success = (Peek() == expected);
+  if (success) {
+    Pop();
+  }
+  return success;
+}
+
+bool TLevel3::TryMatchByte(const char *expected) {
+  assert(this);
+  uint8_t dummy;
+  return TryMatchByte(expected, dummy);
+}
+
+bool TLevel3::TryMatchByte(const char *expected, uint8_t &match) {
+  assert(this);
+  assert(&match);
+  uint8_t c = Peek();
+  bool success = (strchr(expected, c) != nullptr);
+  if (success) {
+    match = c;
+    Pop();
   }
   return success;
 }
