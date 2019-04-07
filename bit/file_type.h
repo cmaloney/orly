de292be37645993ac40b7d1ba3f2d26ce17b7efe
Cody Maloney <cmaloney@theoreticalchaos.com>
2019-04-07 14:56:41 -0700

    WIP

--- bit/file_type.h
+++ bit/file_type.h
@@ -4,12 +4,11 @@ namespace Bit {
 
 // TODO(cmaloney): Allow adding arbitrary file types later, map from string of file type -> integer. Use integers everywhere.
 enum class TFileType {
-  Executable,
-  Object,
-  CSource,
-  CppSource,
-  DependencyFile,
-  Unset
+  Executable = 1,
+  Object = 2,
+  CSource = 3,
+  CppSource = 4,
+  DependencyFile = 5
 };
 
 }
